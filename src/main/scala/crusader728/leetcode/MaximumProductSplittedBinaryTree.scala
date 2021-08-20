e55778bb674f7df586c6e9ee485e714cb09a6d2d
Yiqi Lu <luyiqi@gmail.com>
2021-08-19 22:57:26 -0700

    Update MaximumProductSplittedBinaryTree.scala

--- src/main/scala/crusader728/leetcode/MaximumProductSplittedBinaryTree.scala
+++ src/main/scala/crusader728/leetcode/MaximumProductSplittedBinaryTree.scala
@@ -4,20 +4,26 @@ object MaximumProductSplittedBinaryTree {
   def maxProduct(root: TreeNode): Int = {
     val memo = scala.collection.mutable.HashMap.empty[TreeNode, Int]
 
-    def sum(node: TreeNode): Int = {
-      if (node == null) 0
-      else {
-        val r = node.value + sum(node.left) + sum(node.right)
-        memo += node -> r
-        r
+    def collectSum(t: TreeNode): Int = {
+      t match {
+        case null => 0
+        case _ => memo.getOrElseUpdate(t, t.value + collectSum(t.left) + collectSum(t.right))
       }
     }
 
-    def split(node: TreeNode, total: Int): Long = {
-      if (node == null) 0
-      else List(split(node.left, total), split(node.right, total), memo(node).toLong * (total - memo(node))).max
+    def split(t: TreeNode, total: Int): Long = {
+      t match {
+        case null => 0
+        case n if n.left == null && n.right == null => 0
+        case n if n.left == null => List(split(n.right, total), (total - memo(n.right)).toLong * memo(n.right).toLong).max
+        case n if n.right == null => List(split(n.left, total), (total - memo(n.left)).toLong * memo(n.left).toLong).max
+        case n@_ => List(split(n.right, total),
+          split(n.left, total),
+          (total - memo(n.right)).toLong * memo(n.right).toLong,
+          (total - memo(n.left)).toLong * memo(n.left).toLong).max
+      }
     }
-    val totalSum = sum(root)
-    (split(root, totalSum) % 1000000007).toInt
+
+    (split(root, collectSum(root)) % 1000000007).toInt
   }
 }
