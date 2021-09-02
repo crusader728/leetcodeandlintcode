53cf77a9bbc43d2e374efc4f09060a03df0092bc
Yiqi Lu <luyiqi@gmail.com>
2021-09-01 22:53:57 -0700

    daily challenge 09012021

---
+++ src/main/scala/crusader728/leetcode/ArrayNesting.scala
@@ -0,0 +1,22 @@
+package scala.crusader728.leetcode
+
+object ArrayNesting {
+  def arrayNesting(nums: Array[Int]): Int = {
+    val total = (0 until nums.length - 1).toSet
+
+    @scala.annotation.tailrec
+    def helper(q: Option[Int], total: Set[Int], visited: Set[Int], max: Int): Int = {
+      if (q.isEmpty && total.isEmpty) {
+        Math.max(visited.size, max)
+      } else if (q.isEmpty) {
+        helper(Some(total.head), total, Set.empty, Math.max(visited.size, max))
+      } else {
+        val (newTotal, newVisited) = q.foldLeft((total, visited)) { case ((t, v), i) => (t - i, v + i) }
+        val newQ = q.map(i => nums(i)).filter(i => !newVisited.contains(i))
+        helper(newQ, newTotal, newVisited, max)
+      }
+    }
+
+    helper(Some(nums(0)), total, Set.empty, Int.MinValue)
+  }
+}
