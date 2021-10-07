1ba91257f10d18b6bde735071458392fb9037104
Yiqi Lu <luyiqi@gmail.com>
2021-10-07 13:58:26 -0700

    Update RemoveInvalidParentheses.scala

--- src/main/scala/crusader728/leetcode/RemoveInvalidParentheses.scala
+++ src/main/scala/crusader728/leetcode/RemoveInvalidParentheses.scala
@@ -1,10 +1,14 @@
 package scala.crusader728.leetcode
 
+import scala.util.control.TailCalls._
+
 object RemoveInvalidParentheses {
 
   def removeInvalidParentheses(s: String): List[String] = {
     val (misplacedLeft, misplacedRight) = misplacedCount(s)
-    search(s.toList, misplacedLeft, misplacedRight, 0, Nil, Set.empty).toList
+    search(s.toList, misplacedLeft, misplacedRight, 0, Nil, Set.empty)
+      .map(_.toList)
+      .result
   }
 
   def misplacedCount(s: String): (Int, Int) = {
@@ -26,27 +30,31 @@ object RemoveInvalidParentheses {
              misplacedRight: Int,
              counter: Int,
              result: List[Char],
-             acc: Set[String]): Set[String] = {
+             acc: Set[String]): TailRec[Set[String]] = {
     if (counter < 0) {
-      acc
+      done(acc)
     } else {
       s.headOption match {
         case None => if (misplacedLeft == 0 && misplacedRight == 0 && counter == 0) {
-          acc + result.reverse.mkString
+          done(acc + result.reverse.mkString)
         } else {
-          acc
+          done(acc)
         }
         case Some('(') => if (misplacedLeft == 0) {
           search(s.tail, misplacedLeft, misplacedRight, counter + 1, '(' :: result, acc)
         } else {
-          search(s.tail, misplacedLeft, misplacedRight, counter + 1, '(' :: result, acc)++
-            search(s.tail, misplacedLeft - 1, misplacedRight, counter, result, acc)
+          for {
+            s1 <- search(s.tail, misplacedLeft, misplacedRight, counter + 1, '(' :: result, acc)
+            s2 <- search(s.tail, misplacedLeft - 1, misplacedRight, counter, result, acc)
+          } yield s1 ++ s2
         }
         case Some(')') => if (misplacedRight == 0) {
           search(s.tail, misplacedLeft, misplacedRight, counter - 1, ')' :: result, acc)
         } else {
-          search(s.tail, misplacedLeft, misplacedRight, counter - 1, ')' :: result, acc) ++
-            search(s.tail, misplacedLeft, misplacedRight - 1, counter, result, acc)
+          for {
+            s1 <- search(s.tail, misplacedLeft, misplacedRight, counter - 1, ')' :: result, acc)
+            s2 <- search(s.tail, misplacedLeft, misplacedRight - 1, counter, result, acc)
+          } yield s1 ++ s2
         }
         case Some(ch@_) => search(s.tail, misplacedLeft, misplacedRight, counter, ch :: result, acc)
       }
