6040154898fd5933588abb4f86898ce00f8b37b7
Yiqi Lu <luyiqi@gmail.com>
2021-10-26 11:39:00 -0700

    Update KClosestPointsToOrigin.scala

--- src/main/scala/crusader728/leetcode/KClosestPointsToOrigin.scala
+++ src/main/scala/crusader728/leetcode/KClosestPointsToOrigin.scala
@@ -3,21 +3,25 @@ package scala.crusader728.leetcode
 object KClosestPointsToOrigin {
   type Cord = (Int, Int)
 
-  case class Info(p: Cord, dist: Int)
+  val dist: Cord => Int = { case (x, y) => x * x + y * y }
 
-  implicit val infoOrdering: Ordering[Info] = Ordering.by(info => info.dist)
+  type State = collection.mutable.PriorityQueue[Cord]
+
+  implicit val cordOrdering: Ordering[Cord] = Ordering.by(dist)
+
+  def step(k: Int)(p: Array[Int], state: State): State = {
+    state.enqueue((p(0), p(1)))
+    if(state.size > k) {
+      state.dequeue()
+    }
+    state
+  }
+
 
   def kClosest(points: Array[Array[Int]], k: Int): Array[Array[Int]] = {
-    points
-      .foldRight(collection.mutable.PriorityQueue.empty[Info]) { case (arr, pq) =>
-        val dist = arr(0) * arr(0) + arr(1) * arr(1)
-        pq.enqueue(Info((arr(0), arr(1)), dist))
-        if (pq.size > k) {
-          pq.dequeue
-        }
-        pq
-      }
-      .map(info => Array(info.p._1, info.p._2)).toArray
-
+    val result = points.foldRight(collection.mutable.PriorityQueue.empty[Cord])(step(k))
+    result.map {case (x, y) => Array(x, y) }
+      .toArray
   }
+
 }
