07798735fe7ec9a6e90ab3aec78054780d53c453
Yiqi Lu <luyiqi@gmail.com>
2021-10-27 01:03:32 -0700

    Update ValidateSuduko.scala

--- src/main/scala/crusader728/leetcode/ValidateSuduko.scala
+++ src/main/scala/crusader728/leetcode/ValidateSuduko.scala
@@ -1,22 +1,90 @@
 package scala.crusader728.leetcode
 
 object ValidateSuduko {
-  def isValidSudoku(board: Array[Array[Char]]): Boolean = {
-    def hasNoDuplication(array: Array[Char]): Boolean = {
-      val filtered = array.filter(_ != '.')
-      filtered.distinct.length == filtered.length
+  type Row = Int
+  type Col = Int
+  type Group = Int
+  type Cord = (Int, Int)
+
+  val getRow: Cord => Row = cord => cord._1
+  val getCol: Cord => Col = cord => cord._2
+
+  case class Cell(cord: Cord, value: Option[Int])
+  type Board = Vector[Cell]
+
+  implicit class ToBoardOps(val array: Array[Array[Char]]) extends AnyVal {
+    def toBoard: Board = {
+      (for {
+        i <- array.indices
+        j <- array(i).indices
+      } yield (i, j))
+        .map(cord => {
+          val row = getRow(cord)
+          val col = getCol(cord)
+          val value = array(row)(col)
+          if(value == '.') {
+            Cell(cord, None)
+          } else {
+
+            Cell(cord, Some(value.toInt - '0'.toInt))
+          }
+        }).toVector
     }
+  }
 
-    def submatrices(board: Array[Array[Char]]): Iterator[Array[Char]] =
-      board.grouped(3)
-        .map(_.transpose)
-        .flatMap(_.grouped(3))
-        .map(_.flatten)
+  def validateRow(board: Board, r: Row): Boolean = {
+    (for {
+      i <- 0 until 9
+    } yield r * 9 + i)
+      .map(i => board(i))
+      .map(cell => cell.value)
+      .filter(_.isDefined)
+      .map(_.get)
+      .groupBy(identity)
+      .forall(t => t._2.size == 1)
+  }
 
-    val validateRowResult = board.forall(hasNoDuplication)
-    val validateColumnResult = board.transpose.forall(hasNoDuplication)
-    val validateSubmatrixResult = submatrices(board).forall(hasNoDuplication)
+  def rowValidateMachine(board: Board): Boolean =
+    (0 until 9).map(r => validateRow(board, r)).forall(identity)
 
-    validateColumnResult && validateRowResult && validateSubmatrixResult
+  def validateCol(board: Board, col: Col): Boolean = {
+    (0 until 9)
+      .map(row => row * 9 + col)
+      .map(board(_))
+      .map(_.value)
+      .filter(_.isDefined)
+      .map(_.get)
+      .groupBy(identity)
+      .forall(t => t._2.size == 1)
+  }
+
+  def colValidateMachine(board: Board): Boolean =
+    (0 until 9).map(col => validateCol(board, col)).forall(identity)
+
+  def validateGroup(board: Board, group: Group): Boolean = {
+    (for {
+      i <- 0 until 3
+      j <- 0 until 3
+    } yield ((group / 3) * 3 + i) * 9 + (group % 3) * 3 + j)
+      .map(board)
+      .map(_.value)
+      .filter(_.isDefined)
+      .map(_.get)
+      .groupBy(identity)
+      .forall(t => t._2.size == 1)
+
+  }
+
+  def groupValidateMachine(board: Board): Boolean = {
+    (0 until 9).map(grp => validateGroup(board, grp)).forall(identity)
+  }
+
+  def validateBoard(board: Board): Boolean = {
+    rowValidateMachine(board) && colValidateMachine(board) && groupValidateMachine(board)
+  }
+
+  def isValidSudoku(board: Array[Array[Char]]): Boolean = {
+    val bd = board.toBoard
+    validateBoard(bd)
   }
 }
