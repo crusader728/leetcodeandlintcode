1a38b800dbe27d7899682cf9db481fa2ca27c90d
Yiqi Lu <luyiqi@gmail.com>
2021-10-26 20:36:06 -0700

    Update InvertBinaryTree.scala

--- src/main/scala/crusader728/leetcode/InvertBinaryTree.scala
+++ src/main/scala/crusader728/leetcode/InvertBinaryTree.scala
@@ -2,13 +2,14 @@ package scala.crusader728.leetcode
 
 object InvertBinaryTree {
   import scala.util.control.TailCalls._
+
   def invertTree(root: TreeNode): TreeNode = {
-    def helper(r: TreeNode): TailRec[TreeNode] = {
+    def helper(r: TreeNode): TailRec[TreeNode] = r match {
       case null => done(null)
       case _ => for {
         l <- helper(r.right)
         r <- helper(r.left)
-      } yield new TreeNode(r.value, r, l)
+      } yield new TreeNode(r.value, l, r)
     }
 
     helper(root).result
