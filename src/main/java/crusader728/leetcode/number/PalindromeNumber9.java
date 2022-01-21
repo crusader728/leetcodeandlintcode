2302e9c8156eab44db9fccb1114e700703aca2a2
Yiqi Lu <luyiqi@gmail.com>
2022-01-20 20:11:06 -0800

    Create PalindromeNumber9.java

---
+++ src/main/java/crusader728/leetcode/number/PalindromeNumber9.java
@@ -0,0 +1,20 @@
+package java.crusader728.leetcode.number;
+
+public class PalindromeNumber9 {
+    public boolean isPalindrome(int x) {
+        if(x < 0) {
+            return false;
+        } else if(x < 10) {
+            return true;
+        } else if(x % 10 == 0) {
+            return false;
+        } else {
+            int reverted = 0;
+            while(x > reverted) {
+                reverted = reverted * 10 + x % 10;
+                x = x / 10;
+            }
+            return x == reverted || reverted / 10 == x;
+        }
+    }
+}
