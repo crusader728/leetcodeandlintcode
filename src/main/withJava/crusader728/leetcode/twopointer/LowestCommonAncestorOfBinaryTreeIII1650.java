5a37ada501e34ef7bae7c0f708aa738bf525bcda
Yiqi Lu <luyiqi@gmail.com>
2022-03-19 17:59:19 -0700

    Update LowestCommonAncestorOfBinaryTreeIII1650.java

--- src/main/withJava/crusader728/leetcode/twopointer/LowestCommonAncestorOfBinaryTreeIII1650.java
+++ src/main/withJava/crusader728/leetcode/twopointer/LowestCommonAncestorOfBinaryTreeIII1650.java
@@ -1,20 +1,27 @@
 package withJava.crusader728.leetcode.twopointer;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class LowestCommonAncestorOfBinaryTreeIII1650 {
     public Node lowestCommonAncestor(Node p, Node q) {
-        Node p1 = p;
-        Node p2 = q;
-        while(p1 != p2) {
-            p1 = p1.parent;
-            p2 = p2.parent;
-            if(p1 == null) {
-                p1 = q;
-            }
-            if(p2 == null) {
-                p2 = p;
-            }
+        Deque<Node> pathP = getPath(p);
+        Deque<Node> pathQ = getPath(q);
+        Node prev = null;
+        while(!pathQ.isEmpty() && !pathP.isEmpty() && pathP.peekFirst() == pathQ.peekFirst()) {
+            prev = pathP.poll();
+            pathQ.poll();
         }
-        return p1;
+        return prev;
+    }
+
+    private Deque<Node> getPath(Node q) {
+        Deque<Node> result = new ArrayDeque<>();
+        while(q != null) {
+            result.push(q);
+            q = q.parent;
+        }
+        return result;
     }
 
     private static class Node {
