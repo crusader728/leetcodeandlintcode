2f3f66e15f6eab3b750688be322c998d9b9275b7
Yiqi Lu <luyiqi@gmail.com>
2022-03-19 18:00:24 -0700

    Create CanPlaceFlowers605.java

---
+++ src/main/withJava/crusader728/leetcode/greedy/CanPlaceFlowers605.java
@@ -0,0 +1,19 @@
+package withJava.crusader728.leetcode.greedy;
+
+public class CanPlaceFlowers605 {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) { 
+        int count = 0;
+        for(int i = 0; i < flowerbed.length; ++i) {
+            if(flowerbed[i] == 0) {
+                if((i == 0 || flowerbed[i - 1] == 0) && (i == flowerbed.length - 1 || flowerbed[i + 1] == 0)) {
+                    flowerbed[i] = 1;
+                    count++;
+                }
+                if(count >= n) {
+                    return true;
+                }
+            }
+        }
+        return count >= n;
+    }
+}
