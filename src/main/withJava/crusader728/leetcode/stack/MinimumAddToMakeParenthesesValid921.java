db936dfc2c7a95c07ea7e1ec6cd971b4609f86d1
Yiqi Lu <luyiqi@gmail.com>
2022-03-19 17:59:43 -0700

    Update MinimumAddToMakeParenthesesValid921.java

--- src/main/withJava/crusader728/leetcode/stack/MinimumAddToMakeParenthesesValid921.java
+++ src/main/withJava/crusader728/leetcode/stack/MinimumAddToMakeParenthesesValid921.java
@@ -2,17 +2,13 @@ package withJava.crusader728.leetcode.stack;
 
 public class MinimumAddToMakeParenthesesValid921 {
     public int minAddToMakeValid(String s) {
-        if(s == null || s.isEmpty()) {
-            return 0;
-        }
-
         int left = 0;
         int right = 0;
         for(int i = 0; i < s.length(); ++i) {
             if(s.charAt(i) == '(') {
                 left++;
             } else {
-                if(left > 0) {
+                if(left != 0) {
                     left--;
                 } else {
                     right++;
