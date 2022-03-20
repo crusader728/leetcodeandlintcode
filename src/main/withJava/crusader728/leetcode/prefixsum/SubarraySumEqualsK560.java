da7c4a3742b4f327a7504dce9efa55ccfc467cc8
Yiqi Lu <luyiqi@gmail.com>
2022-03-19 17:59:46 -0700

    Update SubarraySumEqualsK560.java

--- src/main/withJava/crusader728/leetcode/prefixsum/SubarraySumEqualsK560.java
+++ src/main/withJava/crusader728/leetcode/prefixsum/SubarraySumEqualsK560.java
@@ -5,9 +5,9 @@ import java.util.Map;
 
 public class SubarraySumEqualsK560 {
     public int subarraySum(int[] nums, int k) {
-        if (nums == null || nums.length == 0) {
-            return 0;
-        }   
+        if(nums == null || nums.length == 0) {
+            throw new IllegalArgumentException();
+        }
 
         int[] prefixSum = new int[nums.length + 1];
         prefixSum[0] = 0;
@@ -15,18 +15,14 @@ public class SubarraySumEqualsK560 {
             prefixSum[i] = prefixSum[i - 1] + nums[i - 1];
         }
 
-        Map<Integer, Integer> map = new HashMap<>();
         int count = 0;
+        Map<Integer, Integer> prefixSumFreq = new HashMap<>();
         for(int i = 0; i < prefixSum.length; ++i) {
-            if(i == 0) {
-                map.put(prefixSum[i], 1);
-            } else {
-                int other = prefixSum[i] - k;
-                if(map.containsKey(other)) {
-                    count += map.get(other);
-                } 
-                map.put(prefixSum[i], map.getOrDefault(prefixSum[i], 0) + 1);
-            }
+            int other = prefixSum[i] - k;
+            if(prefixSumFreq.containsKey(other)) {
+                count += prefixSumFreq.get(other);
+            } 
+            prefixSumFreq.put(prefixSum[i], prefixSumFreq.getOrDefault(prefixSum[i], 0) + 1);
         }
         return count;
     }
