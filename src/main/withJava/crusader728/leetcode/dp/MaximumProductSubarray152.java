3e557a4fcf07739fd97fb5f5de083d8a1354bdba
Yiqi Lu <luyiqi@gmail.com>
2022-03-30 23:41:08 -0700

    Update MaximumProductSubarray152.java

--- src/main/withJava/crusader728/leetcode/dp/MaximumProductSubarray152.java
+++ src/main/withJava/crusader728/leetcode/dp/MaximumProductSubarray152.java
@@ -2,48 +2,21 @@ package withJava.crusader728.leetcode.dp;
 
 public class MaximumProductSubarray152 {
     public int maxProduct(int[] nums) {
-        if(nums == null || nums.length == 0) {
-            throw new IllegalArgumentException();
+        if(nums.length == 0) {
+            return 0;
         }
 
-        Integer[] dpPositive = new Integer[nums.length];
-        Integer[] dpNegative = new Integer[nums.length];
-
-        for(int i = 0; i < nums.length; ++i) {
-            if(i == 0) {
-                if(nums[i] >= 0) {
-                    dpPositive[i] = nums[i];
-                    dpNegative[i] = null;
-                } else {
-                    dpNegative[i] = nums[i];
-                    dpPositive[i] = null;
-                }
-            } else if(nums[i] >= 0) {
-                dpPositive[i] = dpPositive[i - 1] == null ? nums[i] : Math.max(nums[i], dpPositive[i - 1] * nums[i]);
-                if(dpNegative[i - 1] == null || dpNegative[i - 1] * nums[i] == 0) {
-                    dpNegative[i] = null;
-                } else {
-                    dpNegative[i] = dpNegative[i - 1] * nums[i];
-                }
-            } else {
-                dpPositive[i] = dpNegative[i - 1] == null ? null : dpNegative[i - 1] * nums[i];
-                if(dpPositive[i - 1] == null || dpPositive[i - 1] * nums[i] == 0) {
-                    dpNegative[i] = nums[i];
-                } else {
-                    dpNegative[i] = dpPositive[i - 1] * nums[i];
-                }
-            }
+        int max = nums[0];
+        int min = nums[0];
+        int result = max;
+        for(int i = 1; i < nums.length; ++i) {
+            int current = nums[i];
+            int tempMax = Math.max(current, Math.max(max * current, min * current));
+            min = Math.min(current, Math.min(max * current, min * current));
+            max = tempMax;
+            result = Math.max(result, max);
         }
-        int max = Integer.MIN_VALUE;
-        for(int i = 0; i < nums.length; ++i) {
-            if(dpPositive[i] != null) {
-                max = Math.max(max, dpPositive[i]);
-            } 
-            if(dpNegative[i] != null) {
-                max = Math.max(max, dpNegative[i]);
-            }
-        }
-        return max;
+        return result;
     }
 
     public static void main(String[] args) {
