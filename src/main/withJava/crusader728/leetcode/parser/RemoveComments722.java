ae1ae4afbbc96bde9cf72513e0d36fa6743dbc4d
Yiqi Lu <luyiqi@gmail.com>
2022-04-15 13:55:12 -0700

    Update RemoveComments722.java

--- src/main/withJava/crusader728/leetcode/parser/RemoveComments722.java
+++ src/main/withJava/crusader728/leetcode/parser/RemoveComments722.java
@@ -4,70 +4,118 @@ import java.util.*;
 
 public class RemoveComments722 {
     public List<String> removeComments(String[] source) {
+        if(source == null) {
+            return Collections.emptyList();
+        }
         List<String> result = new ArrayList<>();
-        State currentState = State.NORMAL;
-        StringBuilder builder = new StringBuilder();
+        State state = new Text(new StringBuilder());
         for(String line: source) {
-            for(int i = 0; i < line.length();) {
-                switch(currentState) {
-                    case IN_LINE_COMMENT:
-                        if(i == line.length() - 1) {
-                            currentState = State.NORMAL;
-                        }
-                        i++;
-                        break;
-                    case IN_BLOCK_COMMENT:
-                        if(i < line.length() - 1 && line.charAt(i) == '*' && line.charAt(i + 1) == '/') {
-                            currentState = State.NORMAL;
-                            i = i + 2;
-                        } else {
-                            i++;
-                        }
-                        break;
-                    case NORMAL:
-                        if(i < line.length() - 1 && line.charAt(i) == '/' && line.charAt(i + 1) == '/') {
-                            currentState = State.IN_LINE_COMMENT;
-                            i = i + 2;
-                        } else if(i < line.length() - 1 && line.charAt(i) == '/' && line.charAt(i + 1) == '*') {
-                            currentState = State.IN_BLOCK_COMMENT;
-                            i = i + 2;
-                        } else if(line.charAt(i) == '"') {
-                            currentState = State.IN_STRING;
-                            builder.append(line.charAt(i));
-                            i++;
-                        } else {
-                            builder.append(line.charAt(i));
-                            i++;
-                        }
-                        break;
-                    case IN_STRING:
-                        if(i < line.length() - 1 && line.charAt(i) == '\\' && line.charAt(i + 1) == '"') {
-                            builder.append("\"");
-                            i += 2;
-                        } else if(line.charAt(i) == '"') {
-                            currentState = State.NORMAL;
-                            builder.append('"');
-                            i += 1;
-                        } else {
-                            builder.append(line.charAt(i));
-                            i += 1;
-                        }
-                    default:
-                        break;
-                }
+            Output output = state.read(line, 0);
+            if(!(output.builder.length() == 0)) {
+                result.add(output.builder.toString());
             }
-            if(!currentState.equals(State.IN_BLOCK_COMMENT)) {
-                result.add(builder.toString());
-                builder.delete(0, builder.length());
-            }
+            state = output.state;
         }
         return result;
     }
 
-    private static enum State {
-        IN_LINE_COMMENT,
-        IN_BLOCK_COMMENT,
-        IN_STRING,
-        NORMAL
+    private static class Output {
+        Output() {
+            
+        }
+        State state;
+        StringBuilder builder;
+    }
+    private static interface State {
+        Output read(String line, int idx);
+    }
+
+    private final static class Text implements State {
+        StringBuilder builder;
+
+        public Text(StringBuilder stringBuilder) {
+            this.builder = stringBuilder;
+        }
+
+        @Override
+        public Output read(String line, int idx) {
+            int i = idx;
+            while(i < line.length()) {
+                if(i < line.length() - 1 && line.charAt(i) == '/' && line.charAt(i + 1) == '*') {
+                    //block 
+                    InBlock inBlock = new InBlock(this.builder);
+                    return inBlock.read(line, i + 2);
+                } else if(i < line.length() - 1 && line.charAt(i) == '/' && line.charAt(i + 1) == '/') {
+                    //line comment
+                    Output output = new Output();
+                    output.builder = this.builder;
+                    output.state = new Text(new StringBuilder());
+                    return output;
+                } else if(line.charAt(i) == '"') {
+                    builder.append('"');
+                    InQuote inQuote = new InQuote(this.builder);
+                    return inQuote.read(line, i + 1);
+                } else {
+                    builder.append(line.charAt(i));
+                    i++;
+                }
+            }
+            Output output = new Output();
+            output.builder = this.builder;
+            output.state = new Text(new StringBuilder());
+            return output;
+
+        }
+    }
+
+
+
+    private final static class InBlock implements State {
+        public InBlock(StringBuilder builder) {
+            this.builder = builder;
+        }
+
+        private StringBuilder builder;
+
+        @Override
+        public Output read(String line, int idx) {
+            int i = idx;
+            while(i < line.length()) {
+                if(i < line.length() - 1 && line.charAt(i) == '*' && line.charAt(i + 1) == '/') {
+                    Text text = new Text(this.builder);
+                    return text.read(line, i + 2);
+                } else {
+                    i++;
+                }
+            }
+            Output output = new Output();
+            output.builder = new StringBuilder();
+            output.state = this;
+            return output;
+        }
+
+
+    }
+
+    private final static class InQuote implements State {
+        private StringBuilder builder;
+        public InQuote(StringBuilder builder) {
+            this.builder = builder;
+        }
+
+        @Override
+        public Output read(String line, int idx) {
+            int i = idx;
+            while(i < line.length()) {
+                builder.append(line.charAt(i));
+                if(line.charAt(i) == '"') {
+                    Text text = new Text(this.builder);
+                    return text.read(line, i + 1);
+                } else {
+                    i++;
+                }
+            }
+            throw new IllegalStateException();
+        }
     }
 }
