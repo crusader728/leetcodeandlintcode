4f9dbe7a6a13cc6249e33824c834c243b39d8c38
Yiqi Lu <luyiqi@gmail.com>
2022-04-15 13:53:51 -0700

    Update SearchInRotatedSortedArray33.java

--- src/main/withJava/crusader728/leetcode/binarysearch/SearchInRotatedSortedArray33.java
+++ src/main/withJava/crusader728/leetcode/binarysearch/SearchInRotatedSortedArray33.java
@@ -7,29 +7,29 @@ public class SearchInRotatedSortedArray33 {
         }
 
         int l = 0;
-        int r = nums.length - 1;
-        while(l <= r) {
+        int r = nums.length;
+        while(l < r) {
             int mid = l + (r - l) / 2;
-            if(nums[mid] == target) {
-                return mid;
-            } else {
-                if(nums[mid] < nums[l]) {
-                    if(target < nums[mid]) {
-                        r = mid - 1;
-                    } else if(target > nums[mid] && target <= nums[r]) {
-                        l = mid + 1;
-                    } else {
-                        r = mid - 1;
-                    }
+            if(nums[mid] < nums[l]) {
+                if(nums[mid] < target && target < nums[l]) {
+                    l = mid + 1;
                 } else {
-                    if(target < nums[mid] && nums[l] <= target) {
-                        r = mid - 1;
-                    } else {
-                        l = mid + 1;
-                    }
+                    r = mid;
+                }
+            } else {
+                if(nums[mid] < target) {
+                    l = mid + 1;
+                } else if(nums[mid] >= target && target >= nums[l]) {
+                    r = mid;
+                } else {
+                    l = mid + 1;
                 }
             }
         }
-        return -1;
+        if(l == nums.length) {
+            return -1;
+        } else {
+            return nums[l] == target ? l : -1;
+        }
     }
 }
